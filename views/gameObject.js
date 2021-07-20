24dec035152f9e9e57182d5c8f37b620acfac43d
heohyeonjin <72252806+heohyeonjin@users.noreply.github.com>
2021-07-20 23:04:09 +0900

    Add files via upload

---
+++ views/gameObject.js
@@ -0,0 +1,69 @@
+
+        class PlayerBall{
+            constructor(id,x,y,name,department,phone,comment){
+                this.id = id;
+                this.x = x;
+                this.y = y;
+                this.state = 1;
+                this.playerSpeed = 4;
+                this.name = name;
+                this.department=department;
+                this.phone=phone;
+                this.comment=comment;
+            }
+            getId(){
+                return this.id;
+            }
+            setId(id){
+                this.id = id;
+            }
+            getname(){
+                return this.name;
+            }
+            setname(name){
+                this.name = name;
+            }
+            getdepartment(){
+                return this.department;
+            }
+            setdepartment(department){
+                this.department = department;
+            }
+            getphone(){
+                return this.phone;
+            }
+            setphone(phone){
+                this.phone = phone;
+            }
+            getcomment(){
+                return this.comment;
+            }
+            setcomment(comment){
+                this.comment = comment;
+            }
+            getX(){
+                return this.x;
+            }
+            setX(x){
+                this.x = x;
+            }
+            getY(){
+                return this.y;
+            }
+            setY(y){
+                this.y = y;
+            }
+            getColor(){
+                return this.color;
+            }
+            setColor(color){
+                this.color = color;
+            }
+        
+            getPlayerSpeed(){
+                return this.playerSpeed;
+            }
+            setPlayerSpeed(playerSpeed){
+                this.playerSpeed = playerSpeed;
+            }
+        }
