6407dc9d85156dc81a24424c71ea7908442b935f
Doyun Lee <doyun7433@naver.com>
2021-07-21 20:47:32 +0900

    Add files via upload
    Add room detection

---
+++ views/rooms.js
@@ -0,0 +1,30 @@
+// var room301 = new Room(301, 469, 780, 261, 88);
+// var room309 = new Room(309, 731, 780, 270, 88);
+// var room317 = new Room(317, 1002, 780, 202, 88);
+// var room322 = new Room(322, );
+// var room323 = new Room(323, 1204, 780, 49, 88);
+// var room324 = new Room(324, );
+// var room325 = new Room(325, 1253, 780, 106, 88);
+// var room333 = new Room(333, 1359, 780, 102, 88);
+// var room334 = new Room(334, );
+// var room342 = new Room(342, );
+// var room345 = new Room(345, );
+// var room348 = new Room(348, );
+// var room351 = new Room(351, );
+// var room354 = new Room(354, );
+// var room355 = new Room(355, );
+
+window.onload = function() {
+    let room301 = new Room(301, 469, 780, 261, 88);
+    let room309 = new Room(309, 731, 780, 270, 88);
+    let room317 = new Room(317, 1002, 780, 202, 88);
+    var room323 = new Room(323, 1204, 780, 49, 88);
+    var room325 = new Room(325, 1253, 780, 106, 88);
+    var room333 = new Room(333, 1359, 780, 102, 88);
+    rooms.push(room301);
+    rooms.push(room309);
+    rooms.push(room317);
+    rooms.push(room323);
+    rooms.push(room325);
+    rooms.push(room333);
+}
